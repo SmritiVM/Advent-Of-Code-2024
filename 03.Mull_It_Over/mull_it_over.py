6f605730a8d5f404783c0943e0a12fc38b664f41
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-12-09 20:54:05 +0530

    use constant instead of function parameter

--- 03.Mull_It_Over/mull_it_over.py
+++ 03.Mull_It_Over/mull_it_over.py
@@ -7,12 +7,12 @@ def get_multiplication_result(instruction):
     numbers = re.findall("[0-9]+", instruction)
     return int(numbers[0]) * int(numbers[1])
 
-def find_uncorrupted_mul(program, enable):
+def find_uncorrupted_mul(enable):
     if not enable:
         pattern = "mul\([0-9]+,[0-9]+\)"
-        return re.findall(pattern, program)
+        return re.findall(pattern, PROGRAM)
     pattern = "do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)"
-    instructions = re.findall(pattern, program)
+    instructions = re.findall(pattern, PROGRAM)
     uncorrupted_muls = list()
     for instruction in instructions:
         if instruction == "don't()": enable = False
@@ -21,12 +21,12 @@ def find_uncorrupted_mul(program, enable):
             if enable: uncorrupted_muls.append(instruction)
     return uncorrupted_muls
 
-def sum_of_uncorrupted_mul(program, enable = False):
-    instructions = find_uncorrupted_mul(program, enable)
+def sum_of_uncorrupted_mul(enable = False):
+    instructions = find_uncorrupted_mul(enable)
     return sum([get_multiplication_result(instruction) for instruction in instructions])
 
 
 with open("./03.Mull_It_Over/input.txt") as file:
     PROGRAM = parse_input(file)
-    print(sum_of_uncorrupted_mul(PROGRAM)) 
-    print(sum_of_uncorrupted_mul(PROGRAM, enable = True)) 
+    print(sum_of_uncorrupted_mul()) 
+    print(sum_of_uncorrupted_mul(enable = True)) 
