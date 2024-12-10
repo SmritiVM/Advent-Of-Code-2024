1524c241028b124a77b6f5204c7a99e5b8b11fc9
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-12-10 14:28:55 +0530

    add part 2 and clean up code

--- 10.Hoof_It/hoof_it.py
+++ 10.Hoof_It/hoof_it.py
@@ -1,34 +1,31 @@
 # First find trail heads, for each trail head calculate the score
 from collections import deque
 
-def parse_input(file):
+def parse_input(file: any) -> list:
     return [list(map(int, list(line.strip()))) for line in file]
 
-def out_of_bounds(x, y):
+def out_of_bounds(x: int, y: int) -> bool:
     return x >= ROWS or x < 0 or y >= COLUMNS or y < 0
 
-def find_score(trailhead):
+def find_score_and_rating(trailhead: tuple) -> tuple:
     directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    score = 0
+    rating = 0
     queue = deque([trailhead])
-    visited = {trailhead,}
+    visited_ends = set()
     while queue:
-        # print(queue)
         x, y = queue.popleft()
         for direction in directions:
             dx, dy = direction
             next_x, next_y = x + dx, y + dy
-            # print((next_x, next_y), end = " ")
             if out_of_bounds(next_x, next_y): continue
-            if (next_x, next_y) in visited: continue
             if TOPOGRAPHIC_MAP[next_x][next_y] - TOPOGRAPHIC_MAP[x][y] == 1:
-                if TOPOGRAPHIC_MAP[next_x][next_y] == 9: score += 1
+                if TOPOGRAPHIC_MAP[next_x][next_y] == 9: 
+                    rating += 1
+                    visited_ends.add((next_x, next_y))
                 else: queue.append((next_x, next_y))
-                visited.add((next_x, next_y))
-        # print()
-    return score
+    return len(visited_ends), rating
 
-def find_trailheads():
+def find_trailheads() -> list:
     trailheads = list()
     for x in range(ROWS):
         for y in range(COLUMNS):
@@ -36,18 +33,17 @@ def find_trailheads():
                 trailheads.append((x, y))
     return trailheads
 
-def find_trailhead_score_sum():
+def find_trailhead_score_and_rating_sum() -> tuple:
     trailheads = find_trailheads()
-    total = 0
+    total_score = total_rating = 0
     for trailhead in trailheads:
-        total += find_score(trailhead)
-        # print(find_score(trailhead))
-    return total
+        score, rating = find_score_and_rating(trailhead)
+        total_score += score
+        total_rating += rating
+    return total_score, total_rating
 
-with open("./10.Hoof_It/input.txt") as file:
+with open("./10.Hoof_It/sample_input.txt") as file:
     TOPOGRAPHIC_MAP = parse_input(file)
     ROWS, COLUMNS = len(TOPOGRAPHIC_MAP), len(TOPOGRAPHIC_MAP[0])
-    # print(TOPOGRAPHIC_MAP)
-    # print(find_score((0, 2)))
-    print(find_trailhead_score_sum())
+    print(find_trailhead_score_and_rating_sum()) # Both Part 1 and Part 2 answers returned as a tuple
     
