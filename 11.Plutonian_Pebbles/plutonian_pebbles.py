75d53c48f21d33f92d1860b4ba7fcf63fb65d763
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-12-11 11:39:17 +0530

    optimized part 1 and 2

--- 11.Plutonian_Pebbles/plutonian_pebbles.py
+++ 11.Plutonian_Pebbles/plutonian_pebbles.py
@@ -1,26 +1,28 @@
+import functools
+from collections import Counter 
+
 def parse_input(file):
-    return list(map(int, file.read().strip().split()))
+    return Counter(map(int, file.read().strip().split()))
 
 def blink(stones):
-    new_arrangement = list()
-    for stone in stones:
-        if stone == 0: new_arrangement.append(1)
+    new_arrangement = Counter()
+    for stone, frequency in stones.items():
+        if stone == 0: new_arrangement[1] += frequency
         elif len(str(stone)) % 2 == 0: 
             n = len(str(stone))
-            new_arrangement.extend([int(str(stone)[:n//2]), int(str(stone)[n//2:])])
-        else: new_arrangement.append(stone * 2024)
+            new_arrangement[int(str(stone)[:n//2])] += frequency
+            new_arrangement[int(str(stone)[n//2:])] += frequency
+        else: new_arrangement[stone * 2024] += frequency
     return new_arrangement
 
-def find_stones_after_n_blinks(n, stones):
+def find_stones_after_n_blinks(n: int, stones: Counter):
     blink_count = 0
     while blink_count < n:
         stones = blink(stones)
         blink_count += 1
-        print(blink_count)
-        # print(stones)
-    return len(stones)
+    return sum(stones.values())
 
 with open("./11.Plutonian_Pebbles/input.txt") as file:
     STONES = parse_input(file)
-    # print(STONES)
     print(find_stones_after_n_blinks(25, STONES))
+    print(find_stones_after_n_blinks(75, STONES))
